1c04bd06071e0a4efcb242e7a8c85323cd66a3d7
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-24 00:16:48 +0530

    java basic

--- JAVALANG/src/basic/dataType.java
+++ JAVALANG/src/basic/dataType.java
@@ -1,21 +1,26 @@
 package basic;
 
+import java.util.Scanner;
+
 public class dataType {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         String name = "kishan rana ghosh";
         System.out.println(name);
 
 //        double a = 1/22222222222f;
         int a = 5;
         double b = 2.0;
-        System.out.println(a / b);
+//        System.out.println(a / b);
+//        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.parseInt(sc.nextLine());
+        sc.nextLine(); // consume the next line ( Enter Key )
 
 
 
 
 
 
-        System.out.println(a);
 
 
     }
