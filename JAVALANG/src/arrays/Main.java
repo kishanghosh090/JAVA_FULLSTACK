9ac36d8bcabe730e2f86995a22f2c5b68fa00f1b
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-25 20:04:07 +0530

    add java array

---
+++ JAVALANG/src/arrays/Main.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.sql.Array;
+
+public class Main {
+    public static void main(String[] args){
+        int[] arr1 = {1,23,23,};
+        int[] arr2; // declare but not taking space into the array
+        arr2 = new int[10];
+
+        int[] arr3 = new int[19];
+        for (int i = 0; i < arr1.length; i++){
+            System.out.println(arr1[i]);
+        }
+
+    }
+}
