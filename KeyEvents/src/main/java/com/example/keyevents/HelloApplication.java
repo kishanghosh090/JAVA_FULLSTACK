4053ee58c8737575dfe8cb0e35c025aeb094d9fc
kishanghosh090 <kishanghosh090@gmail.com>
2025-10-02 13:15:17 +0530

    radd events

--- KeyEvents/src/main/java/com/example/keyevents/HelloApplication.java
+++ KeyEvents/src/main/java/com/example/keyevents/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.keyevents;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,19 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        VBox root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+
+        scene.setOnKeyPressed(keyEvent->{
+            KeyController controller = fxmlLoader.getController();
+            controller.handleKeyPressed(keyEvent);
+        });
+        root.requestFocus();
+
+
         stage.setTitle("Hello!");
+
         stage.setScene(scene);
         stage.show();
     }
--- KeyEvents/src/main/java/com/example/keyevents/KeyController.java
+++ KeyEvents/src/main/java/com/example/keyevents/KeyController.java
@@ -2,11 +2,23 @@ package com.example.keyevents;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 
- class KeyController {
+public class KeyController {
     @FXML
     private Label welcomeText;
 
+    @FXML
+    public void handleKeyPressed(KeyEvent event){
+        switch (event.getCode()){
+            case UP:
+                System.out.println("up button clicked");
+                break;
+            case ENTER:
+                System.out.println("Enter preesed");
+
+        }
+    }
     @FXML
     protected void gotclicked() {
         System.out.println("hello");
