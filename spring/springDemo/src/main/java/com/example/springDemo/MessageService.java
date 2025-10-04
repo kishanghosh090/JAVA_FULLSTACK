85560594cfe7e37c5b37501da56e93e41b89d8bd
kishanghosh090 <kishanghosh090@gmail.com>
2025-10-04 19:33:06 +0530

    add spring bean

---
+++ spring/springDemo/src/main/java/com/example/springDemo/MessageService.java
@@ -0,0 +1,10 @@
+package com.example.springDemo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+    public String getMessage(){
+        return  "hello from chai code";
+    }
+}
--- spring/springDemo/src/main/java/com/example/springDemo/SpringDemoApplication.java
+++ spring/springDemo/src/main/java/com/example/springDemo/SpringDemoApplication.java
@@ -2,12 +2,20 @@ package com.example.springDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoApplication.class, args);
-	}
+		ApplicationContext myApp = SpringApplication.run(SpringDemoApplication.class, args);
+	    for (String beanName: myApp.getBeanDefinitionNames()){
+            System.out.println(beanName);
+        }
+
+        MessageService msgService = myApp.getBean(MessageService.class);
+
+        System.out.println(msgService.getMessage());
+    }
 
 }
